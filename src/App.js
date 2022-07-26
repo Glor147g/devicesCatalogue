b6cc5c17d1fe369809ef19fc15724ab9dfb197b7
Glor147g <gloria_lilley@yahoo.co.uk>
2022-07-26 20:51:23 +0100

    app returned

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./App.css";
 // import SearchForm from './components/form/SearchForm'
-
+// import Inputform from './components/form/Inputform'
 // import Contacts from "./components/form/Contacts/contacts.js";
 
 function App(props) {
